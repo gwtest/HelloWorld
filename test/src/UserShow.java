9a1607889c41e72ad2d9db313180b8f5513f1a9e
HollisGW <gwloveslz@gmail.com>
2014-02-24 18:33:54 +0800

    re

--- test/src/UserShow.java
+++ test/src/UserShow.java
@@ -12,8 +12,8 @@ public class UserShow {
         BufferedReader reader;
         ArrayList<String> uidlist =  new ArrayList<>(); 
         //测试分支
-        
 		try {
+			
 		reader = new BufferedReader(new FileReader("/data1/guowu/tmp"));
         String line = reader.readLine();
         while (line != null) {	
